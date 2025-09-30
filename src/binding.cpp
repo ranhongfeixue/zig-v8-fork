a800aa32d837f44f5dd5161bcec93631432f4179
Karl Seguin <k@openmymind.io>
2025-09-30 16:06:12 +0800

    Revert "fix constness"
    This reverts commit 4e4fdc834a50b9618b67686e46cb12e8ff11cd28.

--- src/binding.cpp
+++ src/binding.cpp
@@ -454,8 +454,8 @@ const v8::ArrayBuffer* v8__ArrayBufferView__Buffer(const v8::ArrayBufferView& se
     return local_to_ptr(ptr_to_local(&self)->Buffer());
 }
 
-size_t v8__ArrayBufferView__ByteLength(const v8::ArrayBufferView& self) { return self.ByteLength(); }
-size_t v8__ArrayBufferView__ByteOffset(const v8::ArrayBufferView& self) { return self.ByteOffset(); }
+size_t v8__ArrayBufferView__ByteLength(v8::ArrayBufferView& self) { return self.ByteLength(); }
+size_t v8__ArrayBufferView__ByteOffset(v8::ArrayBufferView& self) { return self.ByteOffset(); }
 
 // HandleScope
 
