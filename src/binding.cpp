3aa2b39cb1ab588b85970beef5b374effccf1415
Karl Seguin <k@openmymind.io>
2025-11-18 11:47:23 +0800

    Expose Isolate::EnqueueMicrotask to be able to enqueue our own [zig] tasks

--- src/binding.cpp
+++ src/binding.cpp
@@ -388,6 +388,10 @@ void v8__Isolate__SetData(v8::Isolate* self, int idx, void* val) {
     self->SetData(idx, val);
 }
 
+void v8__Isolate__EnqueueMicrotask(v8::Isolate* self, v8::MicrotaskCallback callback, void* data) {
+    self->EnqueueMicrotask(callback, data);
+}
+
 size_t v8__HeapStatistics__SIZEOF() {
     return sizeof(v8::HeapStatistics);
 }
--- src/v8.zig
+++ src/v8.zig
@@ -500,6 +500,10 @@ pub const Isolate = struct {
     pub fn getData(self: Self, idx: u32) ?*anyopaque {
         return c.v8__Isolate__GetData(self.handle, @as(c_int, @intCast(idx)));
     }
+
+    pub fn enqueueMicrotask(self: Self, callback: c.MicrotaskCallback, data: ?*anyopaque) void {
+        c.v8__Isolate__EnqueueMicrotask(self.handle, callback, data);
+    }
 };
 
 pub const HandleScope = struct {
