ec92ec31c73f6bbbffb8ef52208e036698c5db13
Karl Seguin <k@openmymind.io>
2026-07-23 10:51:01 +0800

    expose Value::IsFloat16Array

--- src/binding.h
+++ src/binding.h
@@ -602,6 +602,7 @@ bool v8__Value__IsUint32Array(const Value* self);
 bool v8__Value__IsInt32Array(const Value* self);
 bool v8__Value__IsBigInt64Array(const Value* self);
 bool v8__Value__IsBigUint64Array(const Value* self);
+bool v8__Value__IsFloat16Array(const Value* self);
 bool v8__Value__IsFloat32Array(const Value* self);
 bool v8__Value__IsFloat64Array(const Value* self);
 bool v8__Value__IsArrayBuffer(const Value* self);
